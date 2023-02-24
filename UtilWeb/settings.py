59de03c13dabc99cae56521e1397f6536c3ea70c
6410110710 <6410110710@psu.ac.th>
2023-02-24 18:30:17 +0700

    Login With User Authentication

--- UtilWeb/settings.py
+++ UtilWeb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MyApp'
+    'MyApp',
+    'members',
 ]
 
 MIDDLEWARE = [
