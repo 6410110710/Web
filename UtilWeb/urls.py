916d7b1014c8a1b25755f61d2d3b6bc5d3935aa5
6410110710 <6410110710@psu.ac.th>
2023-02-24 16:48:32 +0700

    Customize Admin Title

--- UtilWeb/urls.py
+++ UtilWeb/urls.py
@@ -6,3 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('MyApp.urls')),
 ]
+
+# Configure Admin Title
+admin.site.site_header = "Administration Page"
+admin.site.site_title = "My Web"
+admin.site.index_title = ""
