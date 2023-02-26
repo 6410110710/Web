0000f3eb11f47314b8014afa9f09d80560dfe9e5
6410110710 <6410110710@psu.ac.th>
2023-02-26 23:21:21 +0700

    Add Extra Registration Fields

--- members/views.py
+++ members/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
+from .forms import RegisterUserForm
+
 
 def login_user(request):
     if request.method == "POST":
@@ -26,7 +28,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -36,5 +38,5 @@ def register_user(request):
             messages.success(request, ('Registration Successful '))
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
     return render(request, 'authenticate/register_user.html', {"form":form,})
